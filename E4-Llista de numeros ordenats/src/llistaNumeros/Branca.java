adf4b69da57727171b16e38731f298b917d0a59c
Andreu <Andreu@Andreu1>
2015-10-15 19:40:08 +0200

    do while 2

---
+++ E4-Llista de numeros ordenats/src/llistaNumeros/Branca.java
@@ -0,0 +1,37 @@
+package llistaNumeros;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Branca {
+
+	public static void main(String[] args) {
+		
+		int numero=39000;
+		boolean trobat=false;
+		boolean sortir = false;
+		
+		int[] array = new int[500000];
+		Random rand = new Random();
+		
+		for (int i = 0; i < array.length; i++) {
+		    array[i] = rand.nextInt(1000000) + 1;
+		}
+		Arrays.sort(array);
+		long startTime = System.nanoTime();
+		int i=0;
+		do{
+			if(array[i]==numero) {
+				trobat=true;
+				sortir = true;
+			}
+			if(array[i]>numero) {
+				sortir=true;
+			}
+			i++;
+		}while(sortir==false && i<array.length);
+		long endTime = System.nanoTime();
+		System.out.println("Ha trobat el nomero? " + trobat);
+		System.out.println("Ha tardat " + (endTime - startTime) + " nanosegons");
+	}
+}
